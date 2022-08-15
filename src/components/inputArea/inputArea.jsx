1206d977a6ecec37566120ccdb8e3a70f468b08f
unknown <Dmis0474@gmail.com>
2022-08-15 17:49:11 +0300

    changed hooks

--- src/components/inputArea/inputArea.jsx
+++ src/components/inputArea/inputArea.jsx
@@ -2,28 +2,36 @@ import React, { useState }  from "react";
 import styles from "./inputArea.module.css"
 
 const InputArea = () => {
+    let deal = '';
+    let dealsArray = [];
+    let allDeals = [];
 
-
-  let dealsArray = []
 
  
    
     const [myVariable, setMyVariable] = useState([]);
 
-   function heandleChange(event) {
-      console.log(event.target.value)
-      setMyVariable(myVariable => {return [...myVariable, ...event.target.value]})
-      
-      // console.log(myVariable)
+   function heandleChange(e) {
+      e.preventDefault();
+      console.log(e.target.value)
+      setMyVariable([...e.target.value])
+      console.log(myVariable.join(''))
     }
+
+    
 
     function handleSubmit(e){
       e.preventDefault();
-      myVariable.join('')
-      dealsArray.push(myVariable)
-      console.log(`итог - ${dealsArray}`)
+      deal = myVariable.join('')
+      dealsArray.push(deal)
+      console.log(dealsArray)
+      const allDeals = dealsArray.map((text) =>
+  <p>{text}</p>)
+      console.log(allDeals)
     }
   
+
+    
  
    
   
@@ -31,10 +39,13 @@ const InputArea = () => {
     <form onSubmit={handleSubmit} className={styles.inputArea}>
       <h3>Введите следующее запланированное действие:</h3>
       <textarea placeholder="Введите следующее дело..." onChange={heandleChange}></textarea>
-      <button onClick={heandleChange}>Добавить дело!</button>
+      <button onClick={handleSubmit}>Добавить дело!</button>
       <button>Вычеркнуть последнее дело!</button>
       <p>You clicked times</p>
-      <p>{dealsArray}</p>
+      <div>
+        Дела:
+        {allDeals}
+      </div>
     </form>
   );
 }
