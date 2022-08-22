f22e91daccddbd2ab4e4408c77bd467eaf09aeef
unknown <Dmis0474@gmail.com>
2022-08-22 17:30:56 +0300

    changed deal list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import styles from "./App.module.css";
 import InputArea from "./components/inputArea/inputArea.jsx";
-import ToDoList from './components/to-do-list/to-do-list'
 
 let App = (props) => {
   return (
     <div className={styles.App}>
         <InputArea/>
-        <ToDoList/>
     </div>
   )
 };
--- src/components/inputArea/inputArea.jsx
+++ src/components/inputArea/inputArea.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useState , useId } from "react";
 import styles from "./inputArea.module.css"
 
 const InputArea = () => {
   const [task, setTask] = useState('');
   const [tasks, setTasks] = useState([]);
-
-
+  // const [deletedTask, deleteTask] = useState([]);
+ 
   const handleChange = (e) => {
     setTask(e.target.value);
     console.log(task);
@@ -13,24 +13,33 @@ const InputArea = () => {
 
 
   function handleSubmit(e) {
+    const generatedId = Math.random().toString(16).slice(2)
     e.preventDefault();
-    setTasks([...tasks, task])
+    const taskObj = {id: generatedId, text: task, done: false} 
+    setTasks([...tasks, taskObj])
     console.log(tasks);
   }
+
+  function handleDelete(e) {
+    console.log(e.target.parentNode.getAttribute('id'))
+    setTasks(tasks.filter((task) => task.id !== e.target.parentNode.getAttribute('id')))
+  }
+
+
+
+ let taskList = tasks.map((task, index) => {
+    return <li id={task.id} key={task.id}>{task.text}<button form={task.id} onClick={handleDelete}>Удалить</button></li>
+  })
 
   return (
     <form onSubmit={handleSubmit} className={styles.inputArea}>
       <h3>Введите следующее запланированное действие:</h3>
       <textarea name="task" placeholder="Введите следующее дело..." onChange={handleChange}></textarea>
-      <button onClick={handleSubmit}>Добавить дело!</button>
-      <button>Вычеркнуть последнее дело!</button>
-      <p>You clicked times</p>
-      <div>
+      <button onSubmit={handleSubmit}>Добавить дело!</button>
+      <ul>
         Дела:
-        {tasks.map(task => {
-          return <div key={task}>{task}</div>
-        })}
-      </div>
+        {taskList}
+      </ul>
     </form>
   );
 }
