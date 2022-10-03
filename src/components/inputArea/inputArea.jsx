2aadbfe9e9b25553694f10a20becd18e31f265d9
unknown <Dmis0474@gmail.com>
2022-10-03 19:17:03 +0300

    separated components

--- src/components/inputArea/inputArea.jsx
+++ src/components/inputArea/inputArea.jsx
@@ -1,15 +1,10 @@
 import React, { useState } from "react";
 import styles from "./inputArea.module.css";
+import TasksArea from "../tasksArea/tasksArea";
 
 const InputArea = () => {
   const [task, setTask] = useState("");
   const [tasks, setTasks] = useState([]);
-  const [checked, setChecked] = useState(false);
-  const [editMode, setEditMode] = useState(false);
-  const [inputValue, setInputValue] = useState("");
-  const [edtiableTaskId, setEdtiableTaskId] = useState("")
-  // const [deletedTask, deleteTask] = useState([]);
-
   const handleChange = (e) => {
     setTask(e.target.value);
     // console.log(task);
@@ -23,95 +18,6 @@ const InputArea = () => {
     setTasks([...tasks, taskObj]);
     setTask("");
   }
-
-  function handleDelete(e) {
-    // console.log(e.target.parentNode.getAttribute('id'))
-    setTasks(
-      tasks.filter((task) => task.id !== e.target.parentNode.getAttribute("id"))
-    );
-  }
-
-  function taskDone(e) {
-    e.target.parentNode.style.textDecorationLine = "line-through";
-  }
-
-  function editSubmit(e) {
-    // берем задачу, которую нужно торедактировать, изменяем текст    
-    const editedTask = tasks.find(task => task.id === e.target.parentNode.getAttribute("id"))
-    editedTask.text = inputValue;
-    console.log('editedTask', editedTask);
-
-    // берем все отсальные задачи, кроме редактируемой
-    const noEditedTasks = tasks.filter((task) => task.id !== e.target.parentNode.getAttribute("id"))
-    console.log('noEditedTasks', noEditedTasks);
-
-    // записываем в tasks
-    setTasks([...noEditedTasks, editedTask]);
-    console.log('tasks after editing', tasks);
-    
-    setEditMode(false)
-
-    // let newTasks = [...tasks];
-    // setTasks(
-    //   newTasks.filter((task) => task.id === e.target.parentNode.getAttribute("id"))
-    // );
-    // console.log(newTasks)
-  }
-
-  let taskList = tasks.map((task, index) => {
-    return (
-      <li id={task.id} key={task.id}>
-        <input
-          type="checkbox"
-          checked={checked}
-          onChange={(event) => {
-            setChecked(event.target.checked);
-            // console.log(event.target.checked);
-          }}
-        />
-        {task.id === edtiableTaskId && editMode ? (
-          <div>
-          <input
-            type="text"
-            defaultValue={task.text}
-            key={task.id}
-            id={task.id}
-            title={task.text}
-            onChange={(event) => setInputValue(event.target.value)}
-          />
-          
-          </div >
-        ) : (
-          <p>{task.text}</p>
-        )}
-        <button onClick={handleDelete}>Удалить</button>
-        <button onClick={taskDone}>Задача выполнена!</button>
-        
-        {editMode ? (
-          <button onClick={(e) => {
-            editSubmit(e);
-            setEditMode(false)
-          }
-
-          }>Запомнить</button>
-        ) : (
-          <button
-            onClick={(e) => {
-              setEditMode(!editMode)
-              setEdtiableTaskId(e.target.parentNode.getAttribute("id"));
-              task = e.target.value
-             
-              
-              // task.id === edtiableTaskId ? setEditMode(!editMode) : setEditMode(editMode);
-            }}
-          >
-            Редактировать задачу
-          </button>
-        )}
-      </li>
-    );
-  });
-
   return (
     <div>
       <form
@@ -130,10 +36,7 @@ const InputArea = () => {
         ></textarea>
         <button>Добавить дело!</button>
       </form>
-      <ul>
-        Дела:
-        {taskList}
-      </ul>
+      <TasksArea tasks = {tasks} task = {task} setTasks ={setTasks}/>
     </div>
   );
 };
